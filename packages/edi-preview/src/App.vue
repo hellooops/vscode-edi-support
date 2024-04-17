687e12ef2aaa7a8c60375b675a9551ee5a661a91
Deric <slxs1415@163.com>
2024-04-17 17:57:12 +0800

    Create edi-preview project.

---
+++ packages/edi-preview/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>edi preview</h1>
+    <span>count: {{ count }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+
+const count = ref(0);
+
+let interval: number | undefined = undefined;
+
+onMounted(() => {
+  interval = setInterval(() => {
+    count.value += 1;
+  }, 1000);
+});
+
+onUnmounted(() => {
+  interval && clearInterval(interval);
+});
+</script>
