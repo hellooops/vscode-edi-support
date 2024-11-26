c8ce6df9c76178021f8f431794fd030f487790d7
Deric <slxs1415@163.com>
2024-11-26 14:22:23 +0800

    In preview, also show separatorsSegment.

--- packages/edi-preview/src/views/Inspector/components/InspectorResultDocument.vue
+++ packages/edi-preview/src/views/Inspector/components/InspectorResultDocument.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="mb-[calc(100vh-28px)]">
+    <InspectorResultSegment
+      v-if="ediDocument.separatorsSegment"
+      :segment="ediDocument.separatorsSegment"
+    />
     <InspectorResultInterchange
       v-for="(interchange, i) in ediDocument.interchanges"
       :key="i"
@@ -10,6 +14,7 @@
 <script setup lang="ts">
 import { EdiDocument } from "@/entities";
 import InspectorResultInterchange from "@/views/Inspector/components/InspectorResultInterchange.vue";
+import InspectorResultSegment from "@/views/Inspector/components/InspectorResultSegment.vue";
 
 defineProps<{
   ediDocument: EdiDocument
