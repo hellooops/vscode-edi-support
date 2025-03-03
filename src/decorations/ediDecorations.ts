0ce9839d6a9b27601398b2edda6ca394d62914ec
Deric <slxs1415@163.com>
2025-03-03 10:54:39 +0800

    Remove useless class EdiDecorationsBeforePositionProvider

--- src/decorations/ediDecorations.ts
+++ src/decorations/ediDecorations.ts
@@ -2,7 +2,6 @@ import * as vscode from "vscode";
 import { IDecorationable } from "../interfaces/decorationable";
 import { EdiDecorationsAfterLineProvider } from "./ediDecorationsAfterLineProvider";
 import { EdiDecorationsBeforeLineProvider } from "./ediDecorationsBeforeLineProvider";
-import { EdiDecorationsBeforePositionProvider } from "./ediDecorationsBeforePositionProvider";
 
 export class EdiDecorationsMgr implements IDecorationable {
 
@@ -11,7 +10,6 @@ export class EdiDecorationsMgr implements IDecorationable {
   constructor() {
     this.decorationsProviders.push(new EdiDecorationsAfterLineProvider());
     this.decorationsProviders.push(new EdiDecorationsBeforeLineProvider());
-    // this.decorationsProviders.push(new EdiDecorationsBeforePositionProvider());
   }
 
   registerDecorations(): vscode.Disposable[] {
