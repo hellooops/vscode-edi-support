8b51d7a610304c84bc1f81ada576995e04793b52
Deric <slxs1415@163.com>
2026-03-23 16:34:58 +0800

    test: cover parser entities behavior

---
+++ src/test/suite/parser/entities.test.ts
@@ -0,0 +1,499 @@
+import * as assert from "assert";
+
+import * as constants from "../../../constants";
+import {
+  DiagnoscticsContext,
+  DiagnosticErrors,
+  DiagnosticErrorSeverity,
+  EdiComment,
+  EdiDocument,
+  EdiDocumentBuilder,
+  EdiDocumentSeparators,
+  EdiElement,
+  EdiFunctionalGroup,
+  EdiInterchange,
+  EdiSegment,
+  EdiTransactionSet,
+  EdiType,
+  ElementType,
+} from "../../../parser/entities";
+import { Conf_Utils } from "../../../interfaces/configurations";
+
+suite("Entities Test Suite", () => {
+  test("EdiComment and EdiSegment should expose formatting, nesting and element lookup helpers", () => {
+    const comment = new EdiComment(0, 8, "// header");
+    const loopSegment = new EdiSegment("LOOP", 0, 0, 0, "");
+    const firstChild = new EdiSegment("N1", 0, 4, 5, "~");
+    const secondChild = new EdiSegment("N2", 5, 9, 5, "~");
+    firstChild.ediReleaseSchemaSegment = { desc: "Name One", purpose: "Primary" } as any;
+    secondChild.ediReleaseSchemaSegment = { desc: "Name Two", purpose: "Secondary" } as any;
+    firstChild.parentSegment = loopSegment;
+    secondChild.parentSegment = loopSegment;
+    loopSegment.Loop = [firstChild, secondChild];
+    loopSegment.comments.push(comment);
+
+    const regularSegment = new EdiSegment("REF", 0, 8, 9, "~");
+    regularSegment.ediReleaseSchemaSegment = { desc: "Reference", purpose: "Lookup" } as any;
+    const element = createElement(regularSegment, "01", "ZZ");
+    const component = createComponent(regularSegment, "01-1", "AA");
+    element.components = [component];
+    regularSegment.elements = [element];
+    regularSegment.isInvalidSegment = true;
+    regularSegment.segmentMaximumOccurrencesExceed = { expect: 1, actual: 2 };
+
+    const context = createContext();
+    const errors = regularSegment.getErrors(context);
+
+    assert.strictEqual(comment.getFormatString(), "// header");
+    assert.strictEqual(comment.toString(), "// header");
+    assert.strictEqual(loopSegment.getDesc(), "Name One");
+    assert.strictEqual(loopSegment.getPurpose(), "Primary");
+    assert.deepStrictEqual(loopSegment.getSegments(true), [firstChild, secondChild]);
+    assert.strictEqual(firstChild.getLevel(), 0);
+    assert.strictEqual(secondChild.getLevel(), 1);
+    assert.ok(loopSegment.getFormatString().includes("// header"));
+    assert.ok(loopSegment.getFormatString().includes("N1~"));
+
+    assert.strictEqual(regularSegment.getDesc(), "Reference");
+    assert.strictEqual(regularSegment.getPurpose(), "Lookup");
+    assert.strictEqual(regularSegment.getElement(1), element);
+    assert.strictEqual(regularSegment.getElement(1, 1), component);
+    assert.strictEqual(regularSegment.getElement(2), null);
+    assert.strictEqual(regularSegment.getElement(1, 2), null);
+    assert.deepStrictEqual(errors.map(error => error.code), [
+      DiagnosticErrors.INVALID_VALUE,
+      DiagnosticErrors.SEGMENT_MAXIMUM_OCCURRENCES_EXCEED,
+    ]);
+    assert.strictEqual(regularSegment.isLoop(), false);
+    assert.strictEqual(regularSegment.isHeaderSegment(), true);
+    assert.strictEqual(regularSegment.getIResult().desc, "Reference");
+    assert.strictEqual(regularSegment.toString(), "REF*ZZ~");
+  });
+
+  test("EdiElement should validate values, delegate composite errors and render identifiers", () => {
+    const segment = new EdiSegment("REF", 0, 8, 9, "~");
+    const componentParent = createElement(segment, "01", "PARENT");
+    const componentChild = createComponent(segment, "01-1", "TOO-LONG");
+    componentChild.ediReleaseSchemaElement = createSchemaElement({
+      id: "C001",
+      minLength: 1,
+      maxLength: 3,
+    });
+    componentParent.components = [componentChild];
+
+    const compositeErrors = componentParent.getErrors(createContext());
+
+    const invalidQualifierElement = createElement(segment, "02", "Z");
+    invalidQualifierElement.ediReleaseSchemaElement = createSchemaElement({
+      id: "E001",
+      required: true,
+      minLength: 2,
+      maxLength: 4,
+      qualifierRef: "Party qualifier",
+      release: "00401",
+      qualifierCodes: [],
+    });
+
+    const requiredElement = createElement(segment, "03");
+    requiredElement.ediReleaseSchemaElement = createSchemaElement({
+      id: "E002",
+      required: true,
+      minLength: 1,
+      maxLength: 3,
+    });
+
+    const vdaElement = createElement(segment, "04", "AB   ");
+    vdaElement.ediReleaseSchemaElement = createSchemaElement({
+      id: "E003",
+      minLength: 1,
+      maxLength: 2,
+    });
+
+    const validCodeElement = createElement(segment, "05", "ZZ");
+    validCodeElement.ediReleaseSchemaElement = createSchemaElement({
+      id: "E004",
+      desc: "Code element",
+      qualifierRef: "Party qualifier",
+      qualifierCodes: [{ value: "ZZ", desc: "Buyer" }],
+    });
+
+    const invalidCodeElement = createElement(segment, "06", "XY");
+    invalidCodeElement.ediReleaseSchemaElement = createSchemaElement({
+      id: "E005",
+      desc: "Code element",
+      qualifierRef: "Party qualifier",
+      qualifierCodes: [{ value: "ZZ", desc: "Buyer" }],
+    });
+
+    const noSchemaElement = createElement(segment, "07", "ANY");
+
+    const invalidErrors = invalidQualifierElement.getErrors(createContext());
+    const requiredErrors = requiredElement.getErrors(createContext());
+    const vdaErrors = vdaElement.getErrors(createContext(EdiType.VDA));
+
+    assert.deepStrictEqual(compositeErrors.map(error => error.code), [DiagnosticErrors.VALUE_TOO_LONG]);
+    assert.strictEqual(invalidQualifierElement.getDesignator(), "REF02");
+    assert.strictEqual(invalidQualifierElement.getDesignatorWithId(), "REF02(E001)");
+    assert.strictEqual(invalidQualifierElement.getIdOrDesignator(), "E001");
+    assert.strictEqual(noSchemaElement.getDesignatorWithId(), "REF07");
+    assert.strictEqual(noSchemaElement.getIdOrDesignator(), "REF07");
+    assert.deepStrictEqual(invalidErrors.map(error => error.code), [
+      DiagnosticErrors.VALUE_TOO_SHORT,
+      DiagnosticErrors.QUALIFIER_INVALID_CODE,
+    ]);
+    assert.strictEqual(requiredErrors[0].code, DiagnosticErrors.VALUE_REQUIRED);
+    assert.deepStrictEqual(vdaErrors, []);
+    assert.strictEqual(componentParent.isComposite(), true);
+    assert.strictEqual(noSchemaElement.isComposite(), false);
+    assert.strictEqual(validCodeElement.getIResult().codeValue, "Buyer");
+    assert.strictEqual(invalidCodeElement.getIResult().codeValue, "Invalid code value");
+    assert.strictEqual(noSchemaElement.getErrors(createContext()).length, 0);
+    assert.strictEqual(validCodeElement.toString(), "*ZZ");
+  });
+
+  test("EdiElement should resolve EDIFACT service qualifier overrides through release fallback", () => {
+    const separators = new EdiDocumentSeparators();
+    const document = new EdiDocument(separators, {
+      interchangeStartSegmentName: "UNB",
+      interchangeEndSegmentName: "UNZ",
+      functionalGroupStartSegmentName: "UNG",
+      functionalGroupEndSegmentName: "UNE",
+      transactionSetStartSegmentName: "UNH",
+      transactionSetEndSegmentName: "UNT",
+    });
+    const interchange = document.startInterchange({ id: "242" }, createSegment("UNB", 0, "'"));
+    const functionalGroup = interchange.startFunctionalGroup({ id: "1" }, createSegment("UNG", 0, "'"));
+    const transactionSet = functionalGroup.startTransactionSet({ id: "0001", release: "D97A", version: "DELFOR" }, createSegment("UNH", 0, "'"));
+    const serviceSegment = interchange.startSegment!;
+    const serviceElement = createElement(serviceSegment, "02", "ZZ");
+    serviceElement.ediReleaseSchemaElement = createSchemaElement({
+      id: "0007",
+      qualifierRef: "Identification code qualifier",
+      release: Conf_Utils.EDIFACT_SERVICE_SCOPE,
+      qualifierCodes: [],
+    });
+    serviceSegment.elements = [serviceElement];
+
+    const messageSegment = new EdiSegment("BGM", 0, 0, 0, "'");
+    transactionSet.addSegment(messageSegment);
+
+    const context = createContext(EdiType.EDIFACT, {
+      edifact: {
+        D97A: {
+          qualifiers: {
+            "Identification code qualifier": {
+              ZZ: "<Custom code>",
+            },
+          },
+        },
+      },
+    });
+
+    document.interchanges = [interchange];
+    interchange.functionalGroups = [functionalGroup];
+    functionalGroup.transactionSets = [transactionSet];
+
+    assert.deepStrictEqual(serviceElement.getErrors(context), []);
+  });
+
+  test("Transaction set, functional group, interchange and document should report structural errors", () => {
+    const { document, interchange, functionalGroup, transactionSet } = createHierarchy();
+
+    const detailSegment = createSegment("BEG", 0, "~");
+    transactionSet.addSegment(detailSegment);
+
+    transactionSet.endSegment = createTrailer("SE", "99", "BAD");
+    functionalGroup.endSegment = createTrailer("GE", "2", "BADGROUP");
+    interchange.endSegment = createTrailer("IEA", "3", "BADINT");
+
+    const context = createContext(EdiType.X12, undefined, {
+      separatorsSegmentName: "ISA",
+      interchangeStartSegmentName: "ISA",
+      interchangeEndSegmentName: "IEA",
+      functionalGroupStartSegmentName: "GS",
+      functionalGroupEndSegmentName: "GE",
+      transactionSetStartSegmentName: "ST",
+      transactionSetEndSegmentName: "SE",
+    });
+
+    const transactionErrors = transactionSet.getSelfErrors(context);
+    const functionalGroupErrors = functionalGroup.getSelfErrors(context);
+    const interchangeErrors = interchange.getSelfErrors(context);
+    const documentErrors = document.getErrors(context);
+
+    assert.deepStrictEqual(transactionErrors.map(error => error.code), [
+      DiagnosticErrors.INVALID_VALUE,
+      DiagnosticErrors.INVALID_VALUE,
+    ]);
+    assert.deepStrictEqual(functionalGroupErrors.map(error => error.code), [
+      DiagnosticErrors.INVALID_VALUE,
+      DiagnosticErrors.INVALID_VALUE,
+    ]);
+    assert.deepStrictEqual(interchangeErrors.map(error => error.code), [
+      DiagnosticErrors.INVALID_VALUE,
+      DiagnosticErrors.INVALID_VALUE,
+    ]);
+    assert.strictEqual(documentErrors.length, 7);
+    assert.strictEqual(transactionSet.getFormattedReleaseAndSchemaString(), "00401 850");
+    assert.strictEqual(transactionSet.getControlCount(), 99);
+    assert.strictEqual(functionalGroup.getControlCount(), 2);
+    assert.strictEqual(interchange.getControlCount(), 3);
+    assert.ok(transactionSet.getFormatString().includes("BEG~"));
+    assert.ok(functionalGroup.getFormatString().includes("ST"));
+    assert.ok(interchange.getFormatString().includes("GS"));
+    assert.ok(document.getFormatString().includes("ISA"));
+  });
+
+  test("Interchange should count transaction sets when the first functional group is fake", () => {
+    const separators = new EdiDocumentSeparators();
+    const document = new EdiDocument(separators, {});
+    const interchange = new EdiInterchange({ id: "242" }, document);
+    const fakeGroup = interchange.startFunctionalGroup({ id: "FG1" }, undefined);
+    fakeGroup.startTransactionSet({ id: "0001", release: "D97A" }, undefined);
+    fakeGroup.startTransactionSet({ id: "0002", release: "D97A" }, undefined);
+
+    assert.strictEqual(fakeGroup.isFake(), true);
+    assert.strictEqual(interchange.getRealControlCount(), 2);
+  });
+
+  test("EdiDocumentBuilder should route segments through configured lifecycle hooks", async () => {
+    const separators = new EdiDocumentSeparators();
+    const builder = new EdiDocumentBuilder(separators, {
+      separatorsSegmentName: "UNA",
+      interchangeStartSegmentName: "UNB",
+      interchangeEndSegmentName: "UNZ",
+      functionalGroupStartSegmentName: "UNG",
+      functionalGroupEndSegmentName: "UNE",
+      transactionSetStartSegmentName: ["UNH"],
+      transactionSetEndSegmentName: ["UNT"],
+    });
+    const hookCalls: string[] = [];
+
+    builder.onParseInterchangeMeta(() => {
+      hookCalls.push("parseInterchange");
+      return { id: "242" };
+    });
+    builder.onParseFunctionalGroupMeta(() => {
+      hookCalls.push("parseGroup");
+      return { id: "1" };
+    });
+    builder.onParseTransactionSetMeta(() => {
+      hookCalls.push("parseTransaction");
+      return { id: "0001", release: "D97A", version: "DELFOR" };
+    });
+    builder.onLoadSchema(async () => {
+      hookCalls.push("loadSchema");
+      return true;
+    });
+    builder.onSchemaLoaded(() => {
+      hookCalls.push("schemaLoaded");
+    });
+    builder.onLoadTransactionSetStartSegmentSchema(async (segment) => {
+      hookCalls.push("loadStartSchema");
+      segment.ediReleaseSchemaSegment = { desc: "Loaded start" } as any;
+      return segment;
+    });
+    builder.onAfterEndTransactionSet(async () => {
+      hookCalls.push("afterEnd");
+    });
+    builder.onUnloadSchema(() => {
+      hookCalls.push("unload");
+    });
+
+    const leadingComment = new EdiComment(0, 8, "// before");
+    builder.addComment(leadingComment);
+    await builder.addSegment(createSegment("UNA", 0, "'"));
+    await builder.addSegment(createSegment("UNB", 0, "'"));
+    await builder.addSegment(createSegment("UNG", 0, "'"));
+    await builder.addSegment(createSegment("UNH", 0, "'"));
+    await builder.addSegment(createSegment("BGM", 0, "'"));
+    await builder.addSegment(createSegment("UNT", 0, "'"));
+    await builder.addSegment(createSegment("UNE", 0, "'"));
+    await builder.addSegment(createSegment("UNZ", 0, "'"));
+    builder.addComment(new EdiComment(9, 16, "// after"));
+
+    const ediDocument = builder.buildEdiDocument();
+    const interchange = ediDocument.interchanges[0];
+    const functionalGroup = interchange.functionalGroups[0];
+    const transactionSet = functionalGroup.transactionSets[0];
+
+    assert.deepStrictEqual(hookCalls, [
+      "parseInterchange",
+      "parseGroup",
+      "parseTransaction",
+      "loadSchema",
+      "loadStartSchema",
+      "schemaLoaded",
+      "afterEnd",
+      "unload",
+    ]);
+    assert.strictEqual(ediDocument.separatorsSegment?.id, "UNA");
+    assert.strictEqual(ediDocument.separatorsSegment?.comments[0], leadingComment);
+    assert.strictEqual(transactionSet.startSegment?.ediReleaseSchemaSegment?.desc, "Loaded start");
+    assert.strictEqual(transactionSet.segments[0].id, "BGM");
+    assert.strictEqual(functionalGroup.endSegment?.id, "UNE");
+    assert.strictEqual(ediDocument.commentsAfterDocument.length, 1);
+  });
+
+  test("EdiDocumentBuilder should auto-start transaction sets when no start segment is configured", async () => {
+    const separators = new EdiDocumentSeparators();
+    const builder = new EdiDocumentBuilder(separators, {
+      transactionSetEndSegmentName: "SE",
+    });
+    const hookCalls: string[] = [];
+
+    builder.onParseTransactionSetMeta(() => {
+      hookCalls.push("parseTransaction");
+      return { id: "AUTO", release: "00401", version: "850" };
+    });
+    builder.onLoadSchema(async () => {
+      hookCalls.push("loadSchema");
+      return true;
+    });
+    builder.onLoadTransactionSetStartSegmentSchema(async (segment) => {
+      hookCalls.push("loadStartSchema");
+      return segment;
+    });
+    builder.onSchemaLoaded(() => {
+      hookCalls.push("schemaLoaded");
+    });
+    builder.onAfterEndTransactionSet(async () => {
+      hookCalls.push("afterEnd");
+    });
+    builder.onUnloadSchema(() => {
+      hookCalls.push("unload");
+    });
+
+    await builder.addSegment(createSegment("BGM", 0, "~"));
+    await builder.addSegment(createTrailer("SE", "1", "AUTO"));
+
+    const ediDocument = builder.buildEdiDocument();
+    const transactionSet = ediDocument.interchanges[0].functionalGroups[0].transactionSets[0];
+
+    assert.deepStrictEqual(hookCalls, [
+      "parseTransaction",
+      "loadSchema",
+      "loadStartSchema",
+      "schemaLoaded",
+      "afterEnd",
+      "unload",
+    ]);
+    assert.strictEqual(transactionSet.startSegment, undefined);
+    assert.strictEqual(transactionSet.segments[0].id, "BGM");
+    assert.strictEqual(transactionSet.endSegment?.id, "SE");
+  });
+});
+
+function createContext(
+  ediType: string = EdiType.X12,
+  customSchemas?: any,
+  standardOptions = {
+    separatorsSegmentName: "ISA",
+    interchangeStartSegmentName: "ISA",
+    interchangeEndSegmentName: "IEA",
+    functionalGroupStartSegmentName: "GS",
+    functionalGroupEndSegmentName: "GE",
+    transactionSetStartSegmentName: "ST",
+    transactionSetEndSegmentName: "SE",
+  },
+): DiagnoscticsContext {
+  return {
+    ediType,
+    customSchemas,
+    standardOptions,
+  };
+}
+
+function createHierarchy() {
+  const separators = new EdiDocumentSeparators();
+  const document = new EdiDocument(separators, {
+    separatorsSegmentName: "ISA",
+    interchangeStartSegmentName: "ISA",
+    interchangeEndSegmentName: "IEA",
+    functionalGroupStartSegmentName: "GS",
+    functionalGroupEndSegmentName: "GE",
+    transactionSetStartSegmentName: "ST",
+    transactionSetEndSegmentName: "SE",
+  });
+  const interchange = document.startInterchange({ id: "INT1" }, createSegment("ISA", 0, "~"));
+  const functionalGroup = interchange.startFunctionalGroup({ id: "GROUP1" }, createSegment("GS", 0, "~"));
+  const transactionSet = functionalGroup.startTransactionSet({ id: "TS1", release: "00401", version: "850" }, createSegment("ST", 0, "~"));
+
+  return {
+    document,
+    interchange,
+    functionalGroup,
+    transactionSet,
+  };
+}
+
+function createSegment(id: string, startIndex: number, delimiter: string): EdiSegment {
+  return new EdiSegment(id, startIndex, startIndex + id.length - 1, id.length, delimiter);
+}
+
+function createTrailer(id: string, count: string, controlId: string): EdiSegment {
+  const segment = createSegment(id, 0, "~");
+  segment.elements = [
+    createElement(segment, "01", count),
+    createElement(segment, "02", controlId),
+  ];
+  return segment;
+}
+
+function createElement(segment: EdiSegment, designatorIndex: string, value?: string): EdiElement {
+  const element = new EdiElement(segment, ElementType.dataElement, 1, value?.length ?? 0, "*", segment.id, segment.startIndex, designatorIndex);
+  element.value = value;
+  return element;
+}
+
+function createComponent(segment: EdiSegment, designatorIndex: string, value?: string): EdiElement {
+  const element = new EdiElement(segment, ElementType.componentElement, 1, value?.length ?? 0, ":", segment.id, segment.startIndex, designatorIndex);
+  element.value = value;
+  return element;
+}
+
+function createSchemaElement({
+  id,
+  desc = "Test element",
+  required = false,
+  minLength = 0,
+  maxLength = 99,
+  qualifierRef = "",
+  release = "00401",
+  qualifierCodes,
+}: {
+  id: string;
+  desc?: string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  qualifierRef?: string;
+  release?: string;
+  qualifierCodes?: Array<{ value: string; desc: string }>;
+}) {
+  return {
+    id,
+    desc,
+    dataType: "AN",
+    required,
+    minLength,
+    maxLength,
+    qualifierRef,
+    definition: "Definition",
+    length: maxLength,
+    _schema: {
+      release,
+      qualifiers: qualifierRef ? { [qualifierRef]: qualifierCodes ?? [] } : {},
+    },
+    getCodes() {
+      return qualifierRef ? (qualifierCodes ?? []).map(code => ({ ...code })) : null;
+    },
+    getCodeOrNullByValue(value: string) {
+      const found = qualifierCodes?.find(code => code.value === value);
+      return found ? { value: found.value, desc: found.desc } : null;
+    },
+    isComposite() {
+      return false;
+    },
+  } as any;
+}
